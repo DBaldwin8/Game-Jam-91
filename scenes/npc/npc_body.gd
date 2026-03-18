a5e2c1b679264deee9e9aa02db6b847380a2442a
Darren <darren_baldwin@live.com>
2026-03-18 14:25:01 +0000

    movement logic changed, within process, <0.1 no movement, >0.9 change direction, old code still in comment.

--- scenes/npc/npc_body.gd
+++ scenes/npc/npc_body.gd
@@ -3,15 +3,49 @@ extends CharacterBody2D
 
 # add move_and_slide() from tut for movement.
 
-# Walking needs to be called here in a _process()
+##Walking needs to be called here in a _process()
 # random number multiplied up to change the x position
+var movement = 0
+var direction = "right"
+var speed = 100
 
-func _on_timer_timeout() -> void:
+func _process(delta: float) -> void:
+#func _on_timer_timeout() -> void:
 	var random_num = randf()
-	if random_num > 0.5:
-		position.x += 50
-		print(position.x)
-	else:
-		position.x -= 50
-		print(position.x)
-	timer.start()
+	#print(random_num)
+	if random_num < 0.1:
+		movement = 0
+	if random_num > 0.9:
+		if direction == "right":
+			direction = "left"
+			movement = speed * -1
+		elif direction == "left":
+			direction = "right"
+			movement = speed * 1
+	print(movement)
+	position.x += movement * delta
+	
+	
+	
+	#if random_num > 0.98:
+		##position.x += 1000 * _current_delta
+		#position.x = move_toward(position.x, position.x+500, _current_delta*100)
+		#print(position.x)
+	#elif random_num > 0.96:
+		##position.x -= 1000 * _current_delta
+		#position.x = move_toward(position.x, position.x-500, _current_delta*100)
+		#print(position.x)
+	#else:
+		#pass
+	#timer.start()
+# If randomNum > 0.1 change direction
+# else change directions
+#func changeDirection():
+	#var speed = 10
+	#var direction = 0
+	#if random_num > 0.9:
+		#if direction >= 0:
+			#direction = speed * _current_delta * 1
+		#if direction < 0:
+			#direction = speed * _current_delta * -1
+	#position.x = direction
