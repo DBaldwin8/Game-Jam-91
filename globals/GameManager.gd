ae5c63cee2f21094d20d15aaa75937732383f0fc
Darren <darren_baldwin@live.com>
2026-03-20 12:27:52 +0000

    signal listener added to _ready in GameManager, needs function call adding on later date

--- globals/GameManager.gd
+++ globals/GameManager.gd
@@ -3,3 +3,8 @@ extends Node
 # Autoload singleton; 
 # tracks and mutates game state only
 # (stolen count, wrong arrests, active thief reference).
+
+
+func _ready() -> void:
+	SignalHub.stolen_painting.connect() 
+	# Needs the function to be called when signal is omitted
